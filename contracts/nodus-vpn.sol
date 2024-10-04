e2ed01a1352c7dd804208072f3e94e8c1554443c
mmdev2003 <mmdev2003@gmail.com>
2024-10-04 11:56:02 +0500

    node-client

--- contracts/nodus-vpn.sol
+++ contracts/nodus-vpn.sol
@@ -7,7 +7,29 @@ import "contracts/nodus-token.sol";
 
 contract NodusVPN is Ownable {
     NodusToken public token;
+
+    mapping (address => uint256) internal clientBalances;
+    mapping (uint256 => address) public nodeOwners;
+    string[] public availableNodes;
+    
+    event SetNode(uint node_id, string node_ip, address node_owner);
+
     constructor() Ownable(msg.sender) {
         token = new NodusToken(msg.sender);
-    }    
+    }
+
+    receive() external payable {
+        clientBalances[msg.sender] = clientBalances[msg.sender] + msg.value;
+    }
+
+    function setNodeIP(string memory _ip) external {
+        nodeOwners[availableNodes.length] = msg.sender;
+        availableNodes.push(_ip);
+        
+        emit SetNode(availableNodes.length, _ip, msg.sender);
+    }
+
+    function getAllNode() external view returns(string[] memory) {
+        return availableNodes;
+    }
 }
