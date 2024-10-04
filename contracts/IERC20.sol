7b577b7acaaf42f7d3e54bddb7719c2a8f2dfa07
mmdev2003 <mmdev2003@gmail.com>
2024-10-04 23:23:42 +0500

    dev

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/IERC20.sol)
+pragma solidity ^0.8.27;
 
-pragma solidity ^0.8.20;
 
 interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -12,6 +11,5 @@ interface IERC20 {
     function transfer(address to, uint256 value) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 value) external returns (bool);
-    
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
--- contracts/nodus-vpn.sol
+++ contracts/nodus-vpn.sol
@@ -1,31 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "contracts/nodus-token.sol";
 
-contract NodusVPN is Ownable {
-    NodusToken public token;
+contract Token is ERC20, Ownable {
+    // Храним токены на балансе этого контракта
+    // Для тестнета сделать управление токенов send(address to) onlyOwner
+    constructor(address _owner) ERC20("Nodus", "NDS") Ownable(msg.sender) {
+        _mint(_owner, 1000 * 10 ** decimals());
+    }
 
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
+
+
+// constant Faucet {
+//     // типо сейл
+// }
+
+contract VPN is Ownable {
+
+    IERC20 public NDS;
+    string[] public availableNodes;
     mapping (address => uint256) internal clientBalances;
     mapping (uint256 => address) public nodeOwners;
-    string[] public availableNodes;
     
     event SetNode(uint node_id, string node_ip, address node_owner);
 
-    constructor() Ownable(msg.sender) {
-        token = new NodusToken(msg.sender);
+    constructor(address _nds_address) Ownable(msg.sender) {
+        NDS = IERC20(_nds_address);
     }
 
     receive() external payable {
-        clientBalances[msg.sender] = clientBalances[msg.sender] + msg.value;
+        bool success = IERC20(NDS).transfer(msg.sender, msg.value);
+        require(success);
+        clientBalances[msg.sender] += msg.value;
+    }
+
+    function getClientBalance(address _client) external view returns(uint256) {
+        return clientBalances[_client];
     }
 
     function setNodeIP(string memory _ip) external {
         nodeOwners[availableNodes.length] = msg.sender;
         availableNodes.push(_ip);
-        
+
         emit SetNode(availableNodes.length, _ip, msg.sender);
     }
 
