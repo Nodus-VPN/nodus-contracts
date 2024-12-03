7bdfcaf73d82fb67c809159da8e233fa49b172ff
mmdev2003 <mmdev2003@gmail.com>
2024-12-03 19:54:02 +0500

    fix swap

--- contracts/swap.sol
+++ contracts/swap.sol
@@ -27,16 +27,36 @@ contract Swap is Ownable {
         tokens[_tokenSymbol] = Token(newToken, _price, _precision);
     }
 
-    function swap(string memory _fromSymbol, string memory _toSymbol, uint _amount) external {
+    function swapTokenToToken(string memory _fromSymbol, string memory _toSymbol, uint _amount) external {
         // _amount уже домножен на token.precision
         require(tokens[_fromSymbol].token.balanceOf(msg.sender) >= _amount, "Insufficient client balance");
-        require(tokens[_toSymbol].token.balanceOf(msg.sender) >= _amount, "Insufficient contract balance");
-
+        
         uint sum = _amount.mul(tokens[_fromSymbol].price); 
         uint amountToGet = sum.div(tokens[_toSymbol].price);
+
+        require(tokens[_toSymbol].token.balanceOf(address(this)) >= amountToGet, "Insufficient contract balance");
 
         tokens[_fromSymbol].token.transferFrom(msg.sender, address(this), _amount);
         tokens[_toSymbol].token.transfer(msg.sender, amountToGet);
     }
 
+    function swapEthToToken(string memory _toSymbol) external payable {
+        require(msg.value <= msg.sender.balance, "Insufficient client balance");
+
+        uint sum = msg.value.mul(PRECISION);
+        uint amountToGet = sum.div(tokens[_toSymbol].price);
+
+        require(tokens[_toSymbol].token.balanceOf(address(this)) >= amountToGet, "Insufficient contract balance");
+        tokens[_toSymbol].token.transfer(msg.sender, amountToGet);
+    }
+
+    function swapTokenToETH(string memory _fromSymbol, uint256 _amount) external {
+        require(tokens[_fromSymbol].token.balanceOf(msg.sender) >= _amount, "Insufficient client balance");
+
+        uint sum = _amount.mul(tokens[_fromSymbol].price);
+        uint amountToGet = sum.div(PRECISION);
+
+        tokens[_fromSymbol].token.transferFrom(msg.sender, address(this), _amount);
+        payable(msg.sender).transfer(amountToGet);
+    }
 } 
